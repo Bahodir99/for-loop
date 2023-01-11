5b9212a8be4606c4d8ae67e016cd3649ad88dc83
bahodir <bahodirataboyev7@gmail.com>
2023-01-11 10:47:59 +0500

    hi

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var elInp = document.querySelector('.text');
+var elBtn = document.querySelector('.btn');
+var elTitle = document.querySelector('.title');
+
+
+elBtn.addEventListener('click', ()=>{
+
+    
+
+})
+
+
